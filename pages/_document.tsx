0d2816d136223bfc30ae4de7c79fa3254c3898e6
lourvens <lourvens.luxamar@gmail.com>
2022-08-18 21:53:13 +0200

    switch to next

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles])
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+          <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300&display=swap" rel="stylesheet" />
+          {CssBaseline.flush()}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
